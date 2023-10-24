228758380ec6930ae6ea9358f4daac6426477ea2
Dustin Zeisler <dustin@zeisler.net>
2023-10-24 16:14:55 -0700

    Update tlru.go
    Add run to interface

--- tlru.go
+++ tlru.go
@@ -77,6 +77,9 @@ type TLRU interface {
 	// Has returns true if the provided keys exists in cache otherwise it returns false
 	Has(key string) bool
 
+	// Run starts the garabage collection daemon
+	Run(context.Context)
+
 	// Shutdown stops the garabage collection daemon
 	Shutdown()
 }
